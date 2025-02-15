719d11ae86b3c86abb3430a33ccde20ab0c1d3f2
Bekir <abubekirkaymaz@gmail.com>
2025-02-16 00:50:24 +0300

    delete

--- P1-KDV Hesaplama ve Doğrulama Aracı/calculate.py
+++
@@ -1,9 +0,0 @@
-#kdv hesapalama  fonksiyonu. 
-
-def calculate_kdv (net_amount, vat_rate):
-  vat_amount = net_amount * (vat_rate / 100)
-  gross_amount = net_amount + vat_amount
-  
-  return gross_amount, vat_amount #kdv dahil tutar
-
-
